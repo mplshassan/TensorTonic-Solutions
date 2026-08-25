ffff9391b7e6ea20fc851d583976717824482e09
Mustafa Hassan <mpls.hassan@gmail.com>
2026-08-25 12:56:32 -0500

    Solve sample-var-std

--- sample-var-std/sample-var-std.py
+++ sample-var-std/sample-var-std.py
@@ -6,12 +6,12 @@ def sample_var_std(x: list) -> dict:
     """
     n = len(x)
     x_bar = float(np.mean(x))
-    sum = 0
+    total = 0
     
     for i in range(n):
-        sum += (x[i] - x_bar)**2
+        total += (x[i] - x_bar)**2
 
-    var = sum / (n - 1)
+    var = total / (n - 1)
     sd = float(np.sqrt(var))
 
     return {"variance": var, "standard_deviation": sd}
